da9c0e498fd6e9e945dba558da79f2e7f065ab69
dmitry <1>
2021-01-17 18:16:33 +0500

    add task async web-server

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import asyncio
 
 import aiohttp
+from aiohttp import web
 
 from money import Money
 
@@ -48,6 +49,85 @@ async def check_changes(**kwargs):
             currency_obj.is_changed = False
 
 
+routes = web.RouteTableDef()
+
+
+@routes.get('/')
+async def index(request):
+    return web.json_response({"status": "OK"})
+
+
+@routes.get('/amount/get')
+async def get_amount(request):
+    currency_objs_list = request.app['currency_objs_list']
+    total_cost_in_rubles = sum(list(map(lambda x: x.get_cost_in_rubles(), currency_objs_list)))
+    ### TODO: переделать без множества циклов
+    ### TODO: добавить rub-usd....
+    response_string = ""
+    for currency_obj in currency_objs_list:
+        response_string += f"{currency_obj.name}: {currency_obj.amount}\n"
+    response_string += f"sum: "
+    for currency_obj in currency_objs_list:
+        response_string += f"/ {round(total_cost_in_rubles / currency_obj.cost, 2)} {currency_obj.name}"
+    return web.Response(text=response_string, content_type='text/plain')
+
+
+@routes.get('/{currency_name}/get')
+async def get_currency_name(request):
+    currency_name = request.match_info['currency_name'].upper()
+    currency_objs_list = request.app['currency_objs_list']
+    current_currency = next(filter(lambda x: x.name == currency_name, currency_objs_list), None)
+    if current_currency is None:
+        return web.Response(text=f'Unknown currency: {currency_name}', status=403)
+    return web.json_response(
+        {"Currency": current_currency.name, "Amount": current_currency.amount, "Cost": current_currency.cost})
+
+
+@routes.post('/amount/set')
+async def set_amount(request):
+    request_body = await request.json()
+    if not request_body:
+        return web.Response(text=f'Unknown request', status=403)
+    currency_objs_list = request.app['currency_objs_list']
+    response = ''
+    for key, value in request_body.items():
+        for currency_obj in currency_objs_list:
+            if key.upper() == currency_obj.name:
+                currency_obj.amount = value
+                currency_obj.is_changed = True
+
+    for currency_obj in currency_objs_list:
+        response += f"{currency_obj}\n"
+
+    return web.json_response(response)
+
+
+@routes.post('/modify')
+async def set_amount(request):
+    request_body = await request.json()
+    if not request_body:
+        return web.Response(text=f'Unknown request', status=403)
+    currency_objs_list = request.app['currency_objs_list']
+    response = ''
+    for k, v in request_body.items():
+        for currency_obj in currency_objs_list:
+            if k.upper() == currency_obj.name:
+                currency_obj.amount += v
+                currency_obj.is_changed = True
+
+    for currency_obj in currency_objs_list:
+        response += f"{currency_obj}\n"
+
+    return web.json_response(response)
+
+
+def get_webserver_settings(**kwargs):
+    app = aiohttp.web.Application()
+    app['currency_objs_list'] = kwargs.get('currency_objs_list')
+    app.add_routes(routes)
+    return app
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--period', action="store", dest="period", required=True, type=int,
@@ -72,6 +152,9 @@ if __name__ == '__main__':
         ),
         loop.create_task(
             repeat(60, check_changes, currency_objs_list=[rub, usd, eur])
+        ),
+        loop.create_task(
+            web.run_app(get_webserver_settings(currency_objs_list=[rub, usd, eur]), host='127.0.0.1', port=8000)
         )
 
     ]
